232eabe4f022830af8ebf082d7413a8f52455fda
Luck Soares <lucasmarcelo93@gmail.com>
2024-08-23 20:37:07 -0300

    Feat: Mobile Nav Menu

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -36,7 +36,20 @@ const MobileNav = () => {
         <SheetTrigger className="flex justify-center items-center">
             <CiMenuFries className="text-[32px] text-accent"/>
         </SheetTrigger>
-        <SheetContent className="bg-primary">Content</SheetContent>
+        <SheetContent className="flex flex-col">
+            {/* Logo */}
+            <div className="mt-32 mb-40 text-center text-2xl">
+                <Link href="/">
+                    <h1 className='text-4xl font-semibold'>Luck<span className='text-accent'>.</span>  </h1>   
+                </Link>    
+            </div>
+            {/*nav */}
+            <nav className='flex flex-col justify-center items-center gap-8'>
+                {links.map((link, index)=>{
+                return <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>{link.name}</Link>
+                 })}
+            </nav>
+        </SheetContent>
     </Sheet>
   )
 }
