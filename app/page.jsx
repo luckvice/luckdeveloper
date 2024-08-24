792b6aeed6202d01487d04139b8255529ecc7bfd
luckvice <lucasmarcelo93@gmail.com>
2024-08-24 11:46:49 -0300

    Feat: Profile Photo

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/Button"
 import {FiDownload} from "react-icons/fi"
 import Social from "@/components/Social"
+import Photo from "@/components/Photo"
+
 
 const Home = () => {
   return (
@@ -8,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between
         xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer, Analista de sistemas</span>
             <h1 className="h1 mb-6">Hello World<br/><span className="text-accent">Luck DEV</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">Me chamo Lucas e crio experiências elegantes, 
@@ -27,7 +29,9 @@ const Home = () => {
                 </div>
               </div>
           </div>
-
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo/>
+          </div>
         </div>
       </div>
     </section>
