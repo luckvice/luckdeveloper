44f4e11e5a201ce9dc1cbad2444a2675920ce747
luckvice <lucasmarcelo93@gmail.com>
2024-08-24 11:05:22 -0300

    Feat: Social icons

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/Button"
 import {FiDownload} from "react-icons/fi"
+import Social from "@/components/Social"
+
 const Home = () => {
   return (
     <section className="h-full">
@@ -16,13 +18,12 @@ const Home = () => {
                 <Button 
                 variant="outline"
                 size="lg"
-                className="uppercase flex items-center gap-2"
-                >
+                className="uppercase flex items-center gap-2">
                   <span className="text-xl">Download CV</span>
                   <FiDownload className="text-xl"/>
                 </Button>
                 <div className="mb-8 xl:mb-0">
-              
+                  <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
                 </div>
               </div>
           </div>
