24c5262bd9ceb50b579ff654a4211e22afede63f
Luck Soares <lucasmarcelo93@gmail.com>
2024-08-24 07:50:20 -0300

    Feat: Button Design

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -46,7 +46,10 @@ const MobileNav = () => {
             {/*nav */}
             <nav className='flex flex-col justify-center items-center gap-8'>
                 {links.map((link, index)=>{
-                return <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>{link.name}</Link>
+                return <Link 
+                    href={link.path} key={index} 
+                    className={`${link.path === pathname && "text-accent border-b-2 border-accent"} 
+                    text-xl capitalize hover:text-accent transition-all`}>{link.name}</Link>
                  })}
             </nav>
         </SheetContent>
