ad0f831033f8b946dab402ac13805db8adb0c5b2
luckvice <lucasmarcelo93@gmail.com>
2024-08-24 10:15:59 -0300

    Feat: Home section I am

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,37 @@
-export default function Home() {
+import { Button } from "@/components/ui/Button"
+import {FiDownload} from "react-icons/fi"
+const Home = () => {
   return (
-    <main>
-    Hello World
-    </main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between
+        xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer, Analista de sistemas</span>
+            <h1 className="h1 mb-6">Hello World<br/><span className="text-accent">Luck DEV</span></h1>
+            <p className="max-w-[500px] mb-9 text-white/80">Me chamo Lucas e crio experiências elegantes, 
+              de nivel intermediário a avançado em várias técnologias</p>
+              {/* btn and social */}
+              <div className="flex flex-col xl:flex-row items-center gap-8">
+                <Button 
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+                >
+                  <span className="text-xl">Download CV</span>
+                  <FiDownload className="text-xl"/>
+                </Button>
+                <div className="mb-8 xl:mb-0">
+              
+                </div>
+              </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  )
 }
+
+export default Home
