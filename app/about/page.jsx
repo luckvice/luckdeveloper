6cecb66657dcf6fc2fe53214d42516502b16cf7c
Luck Soares <lucasmarcelo93@gmail.com>
2024-08-26 20:43:56 -0300

    FIX: Values of about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,6 @@
+"use client"
 import { Description } from "@radix-ui/react-dialog"
-import {FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs} from "react-icons/fa"
+import {FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs, FaFigma} from "react-icons/fa"
 import {SiTailwindcss, SiNextdotjs} from "react-icons/si"
 
 
@@ -66,11 +67,100 @@ const experience = {
   ]
 }
 
+const education = {
+  icon: '/assets/resume/cap.svg',
+  tittle: 'Minhas formações',
+  description: 'todo',
+  items: [
+    {
+      institution: 'Objetus Informatica',
+      degree: 'Técnico em Informática',
+      duration: '2012 - 2012'
+    },
+    {
+      institution: 'Escola Técnica Irmão Pedro',
+      degree: 'Técnico em Publicidade e Propaganda',
+      duration: '2016 - 2017'
+    },
+    {
+      institution: 'SENAC RS',
+      degree: 'Analise e Desenvolvimento de sistemas',
+      duration: '2014 - 2021'
+    },
+  ]
+}
 
+const skills = {
+  title: 'Minhas Skills',
+  description: 'Todo',
+  skillList:[
+    {
+      icon: <FaHtml5/>,
+      name: 'html5'
+    },
+    {
+      icon: <FaCss3/>,
+      name: 'CSS 3'
+    },
+    {
+      icon: <FaJs/>,
+      name: 'Javascript'
+    },
+    {
+      icon: <FaReact/>,
+      name: 'React JS'
+    },
+    {
+      icon: <SiNextdotjs/>,
+      name: 'Next JS'
+    },
+    {
+      icon: <SiTailwindcss/>,
+      name: 'Tailwind.css'
+    },
+    {
+      icon: <FaNodeJs/>,
+      name: 'Node.JS'
+    },
+    {
+      icon: <FaFigma/>,
+      name: 'Figma'
+    },
+  ]
+}
+
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip"
+import {ScrollArea} from "@/components/ui/scroll-area" 
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div>About</div>
+    <motion.div initial={{opacity: 0}} 
+    animate={{
+      opacity:1, transition:
+      {delay:2.4, duration:0.4,ease: 'easeIn'}
+      }} className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0">
+        <div className="container mx-auto">
+          <Tabs defaultValue="experiencia" className="flex flex-col xl:flex-row gap-[60px]">
+            <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+              <TabsTrigger value="experiencia">
+                Experiencia
+              </TabsTrigger>
+              <TabsTrigger value="formacoes">
+                Formações
+              </TabsTrigger>
+              <TabsTrigger value="skills">
+                Skills
+              </TabsTrigger>
+              <TabsTrigger value="about">
+                Sobre mim
+              </TabsTrigger>
+            </TabsList>
+            <div>Content</div>
+          </Tabs>
+        </div>
+    </motion.div>
   )
 }
 
