e1e19f9a5634dbb41013338dbbdcd6aa26a87516
luckvice <lucasmarcelo93@gmail.com>
2024-08-30 09:15:17 -0300

    Feat: In progress about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -142,12 +142,12 @@ const About = () => {
       {delay:2.4, duration:0.4,ease: 'easeIn'}
       }} className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0">
         <div className="container mx-auto">
-          <Tabs defaultValue="experiencia" className="flex flex-col xl:flex-row gap-[60px]">
+          <Tabs defaultValue="experiene" className="flex flex-col xl:flex-row gap-[60px]">
             <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
-              <TabsTrigger value="experiencia">
+              <TabsTrigger value="experiene">
                 Experiencia
               </TabsTrigger>
-              <TabsTrigger value="formacoes">
+              <TabsTrigger value="education">
                 Formações
               </TabsTrigger>
               <TabsTrigger value="skills">
@@ -157,7 +157,9 @@ const About = () => {
                 Sobre mim
               </TabsTrigger>
             </TabsList>
-            <div>Content</div>
+            <div className="min-h-[70vh] w-full">
+              
+            </div>
           </Tabs>
         </div>
     </motion.div>
